df6884ed9c529c39960b78c61c628e6106c4ab84
RielaEstevez <mauroliceo18@gmail.com>
2024-06-14 09:16:53 -0300

    Se pasaron las funciones al archivo procesamiento.php

--- procesamiento.php
+++ procesamiento.php
@@ -1,3 +1,79 @@
 <?php
 
+session_start();
+
+function agregarProducto ($productos, $nombre, $cantidad, $precio, $modelo) {
+    $productos[] = [
+        'nombre' => $nombre, 
+        'cantidad' => $cantidad,
+        'precio' => $precio,
+        'modelo' => $modelo
+    ];
+
+    return $productos;
+}
+
+function buscarPorModelo($productos, $modelo) {
+    foreach($productos as $producto){
+        if($producto['modelo'] == $modelo){
+            return "Nombre: " . $producto['nombre'] . " Cantidad: " . $producto['cantidad'] . " Precio: " . $producto['precio'] . "<br>";
+        }
+    }
+    return "Modelo no encontrado";
+}
+
+function mostrarProductos($productos) {
+    $resultado = '';
+    foreach($productos as $producto) {
+        $resultado .= "Nombre: " . $producto['nombre'] . " Cantidad: " . $producto['cantidad'] . " Precio: " . $producto['precio'] . "<br>";
+    }
+    return $resultado;
+}
+
+function actualizarProducto($productos, $nombre, $cantidad, $precio, $modelo) {
+    foreach($productos as &$producto){
+        if($producto['modelo'] == $modelo){
+            $producto['nombre'] = $nombre;
+            $producto['cantidad'] = $cantidad;
+            $producto['precio'] = $precio;
+            break;
+        }
+    }
+    return $productos;
+}
+
+function calcularValorTotal($productos) {
+    $total = 0;
+    foreach($productos as $producto){
+        $total = $total + $producto['precio'];
+    }
+    return $total;
+}
+
+function filtrarPorValor($productos, $precio) {
+    $resultado = '';
+    foreach($productos as $producto) {
+        if($producto['precio'] > $precio)
+        $resultado .= "Nombre: " . $producto['nombre'] . " Cantidad: " . $producto['cantidad'] . " Precio: " . $producto['precio'] . "<br>";
+    }
+    return $resultado;
+}
+
+function listarModelos($productos){
+    $resultado = '';
+    foreach($productos as $producto){
+        $resultado .= $producto['modelo'] . "<br>";
+    }
+    return $resultado;
+}
+
+function calcularPromedio($productos) {
+    $total = 0;
+    foreach($productos as $producto){
+        $total = $total + $producto['precio'];
+    }
+    $promedio = (float)$total / count($productos);
+    return $promedio;
+}
+
 ?>
