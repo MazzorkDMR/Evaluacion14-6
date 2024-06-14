8c06def40e73ce609455edf201c5251e98d91280
RielaEstevez <mauroliceo18@gmail.com>
2024-06-14 08:17:34 -0300

    Funcion buscarPorModelo creada y probada correctamente

--- pruebaFunciones.php
+++ pruebaFunciones.php
@@ -13,11 +13,27 @@ function agregarProducto ($productos, $nombre, $cantidad, $precio, $modelo) {
     return $productos;
 }
 
+function buscarPorModelo($productos, $modelo){
+    foreach($productos as $producto){
+        if($producto['modelo'] == $modelo){
+            return "Nombre: " . $producto['nombre'] . " Cantidad: " . $producto['cantidad'] . " Precio: " . $producto['precio'] . "<br>";
+        }
+    }
+    return "Modelo no encontrado";
+}
+
 $productos = agregarProducto($productos, "Lavarropas", 66, 14500, "James");
 print_r($productos);
 echo "<br>";
 $productos = agregarProducto($productos, "TV", 3, 12500, "LG");
 print_r($productos);
+
+$resultado = buscarPorModelo($productos, "James");
+echo $resultado;
+
+$resultado = buscarPorModelo($productos, "Logitech");
+echo $resultado;
+echo "<br>";
 
 
 ?>
